0b88cb10c2f7d5165b522d02cc122fa0a3534711
jkuehner <jochen.kuehner@kardex.com>
2023-09-03 16:48:13 +0200

    tab control content should be relaltive

--- src/tab/TabWebcomponent.ts
+++ src/tab/TabWebcomponent.ts
@@ -11,7 +11,7 @@ export class TabWebcomponent extends BaseCustomWebComponentConstructorAppend {
             ::slotted(*) {
                 width: 100%;
                 height: 100%;
-                position: static;
+                position: relative;
             }
 
             .tab-header-container::-webkit-scrollbar { 
